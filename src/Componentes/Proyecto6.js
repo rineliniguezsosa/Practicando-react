a8136b457feaf633af3b3c593cf5a52a10b753bf
rinel <rineliniguezsosa@gmail.com>
2021-07-25 19:55:24 -0500

    color generador

--- src/Componentes/Proyecto6.js
+++ src/Componentes/Proyecto6.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
+import Box from '@material-ui/core/Box'
+import { withStyles } from '@material-ui/core/styles'
+import { useStyles } from '../Styleproyecto6/Style'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+import Values from 'values.js'
 
-//url https://www.thecocktaildb.com/api/json/v1/1/search.php?s=
 class Proyecto6 extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = { 
+            color:"",
+            error:false,
+            lista:[],
+         }
+    }
+    Send = (e) => {
+        e.preventDefault();
+        let colores = new Values(this.state.color).all(10);
+        console.log(colores)
     }
     render() { 
+        const {classes} = this.props
         return ( 
-            <div>
-
-            </div>
+            <Box className={classes.main} style={{width:"100%",height:"560px"}}>
+                <Box className={classes.boxform} >
+                    <span className={classes.texto}>Generador de colores</span>
+                    <form onSubmit={this.Send}>
+                        <TextField size="small" onChange={(e)=> this.setState({ color: e.target.value })} label="#FFFFFF" variant="outlined"></TextField>
+                        <Button type="submit" style={{height:"40px"}} value={this.state.color} variant="contained" color="primary">
+                          agregar
+                        </Button>
+                    </form>
+                </Box>
+            </Box>
          );
     }
 }
  
-export default Proyecto6;
+export default withStyles(useStyles)(Proyecto6);
--- src/Styleproyecto6/Style.js
+++ src/Styleproyecto6/Style.js
@@ -1,4 +1,22 @@
 
 export const useStyles = ()=> ({
-    
+    main:{
+        display:"flex",
+        flexDirection:"column",
+        
+    },
+    boxform:{
+        width:"100%",
+        height:"70px",
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop:"10px",
+    },
+    texto:{
+        fontFamily:"sans-serif",
+        marginRight:"10px",
+        fontWeight:700,
+    },
 })
+
